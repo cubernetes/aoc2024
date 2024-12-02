2d02dc21cc730cfab3782d01f5c0da862314577c
tosu <timo42@proton.me>
2024-12-02 16:31:51 +0100

    Rename s01_cpp23 to s01_cpp23-github and add own s01_cpp23

--- 1/solution1_cpp23.cpp
+++ 1/solution1_cpp23.cpp
@@ -1,39 +1,39 @@
+// run on godbolt: https://godbolt.org/z/7hM3bno3x
+
 #include <algorithm>
 #include <fstream>
-#include <iostream>
-#include <string>
-#include <ranges>
+#include <print>
 #include <vector>
+#include <sstream>
+#include <ranges>
+
+// for easy printing of vectors
+// fmt.dev/latest/get-started
+#include <fmt/ranges.h>
 
 using namespace std;
 
-// uses features from
-// - c++23 (std::views::zip)
-// - c++20 (std::ranges)
-// - c++17 (structured bindings)
-// - c++11 (stoi, auto)
-// stolen from
-// https://github.com/vss2sn/advent_of_code/blob/master/2024/cpp/day_01a.cpp
-// `g++ -std=c++23`
-
 int main() {
-	string input = "test.txt";
-	ifstream file(input);
-	string line;
-	vector<int> l1;
-	vector<int> l2;
+	ifstream s("in.txt");
 
-	while(getline(file, line)) {
-		size_t space_idx = line.find(' ');
-		l1.push_back(stoi(line.substr(0, space_idx)));
-		l2.push_back(stoi(line.substr(space_idx + 1, line.size() - space_idx - 1)));
-	}
-	ranges::sort(l1);
-	ranges::sort(l2);
-	long long sum = 0;
-	for (const auto& [n1, n2] : views::zip(l1, l2)) {
-		sum += abs(n1 - n2);
-	}
-	cout << sum << '\n';
+	vector<pair<int, int>> R, L = R = views::istream<int>(s)
+		| views::chunk(2)
+		| views::transform([](auto&& chunk) -> pair<int, int> {
+				auto it = chunk.begin();
+				return pair{static_cast<int>(*it), *++it};
+			})
+		| ranges::to<vector>();
+
+	// fmt::println("{}", L); // when removing the static_cast above, the tuples in L will have the same 2 values, namely the one from the 2nd dereferencing
+
+	ranges::sort(L, less{}, [](auto&& p){ return get<0>(p); });
+	ranges::sort(R, less{}, [](auto&& p){ return get<1>(p); });
+
+	int sum = ranges::fold_left(views::zip_transform([](auto&& l, auto&& r){
+				return abs(get<0>(l) - get<1>(r));
+			}, L, R),
+		0, plus{});
+
+	println("{}", sum);
 	return 0;
 }
