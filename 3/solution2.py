5b5f213bd364c111bbbdd847525cd61d71dde335
tosu <timo42@proton.me>
2024-12-03 06:16:16 +0100

    03 - s02

---
+++ 3/solution2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import re
+data = open(0).read().strip()
+
+t = 0
+flag = True
+for match in re.finditer(r'(mul\(\d+,\d+\))|(do\(\))|don\'t\(\)', data):
+    m = match.group(0)
+    if m == "don't()":
+        flag = False
+    if m == "do()":
+        flag = True
+    if m.startswith("mul(") and flag:
+        t+= eval(match.group(0).removeprefix('mul(').removesuffix(')').replace(",",'*'))
+print(t)
