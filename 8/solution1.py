fb854dd4c324977469fa54966d7d88613f731b2d
tosu <timo42@proton.me>
2024-12-08 06:53:27 +0100

    08 - s01

---
+++ 8/solution1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env pypy3
+import itertools
+full_grid=list(map(list,open(0).read().splitlines()))
+distincts=set()
+for _, r in enumerate(full_grid):
+    for _, c in enumerate(r):
+        distincts.add(c)
+distincts.remove('.')
+grids=[]
+for d in distincts:
+    grids.append(([], d))
+    for i, r in enumerate(full_grid):
+        grids[-1][0].append([])
+        for j, c in enumerate(r):
+            if c in '.' + d:
+                grids[-1][0][-1].append(c)
+            else:
+                grids[-1][0][-1].append('.')
+def count_antinodes(grid, d) -> set[tuple[int, int]]:
+    positions = []
+    for i, r in enumerate(grid):
+        for j, c in enumerate(r):
+            if c == d:
+                positions.append((i, j))
+    antis = set()
+    for a1, a2 in itertools.combinations(positions, 2):
+        di = a2[0] - a1[0]
+        dj = a2[1] - a1[1]
+        anti1i = a2[0] + di
+        anti1j = a2[1] + dj
+        anti2i = a1[0] - di
+        anti2j = a1[1] - dj
+        if 0 <= anti1i <= len(grid) - 1 and 0 <= anti1j <= len(grid[0]) - 1:
+            antis.add((anti1i, anti1j))
+        if 0 <= anti2i <= len(grid) - 1 and 0 <= anti2j <= len(grid[0]) - 1:
+            antis.add((anti2i, anti2j))
+    return antis
+# anti_positions = [(x[0], x[1]) for x in all_antis]
+# for i, r in enumerate(full_grid):
+#     for j, c in enumerate(r):
+#         if (i, j) in anti_positions:
+#             print(end='#')
+#         else:
+#             print(end=c)
+#     print()
+all_antis = set()
+for grid_pair in grids:
+    grid, d = grid_pair
+    antis = count_antinodes(grid, d)
+    all_antis = all_antis | antis
+print(len(all_antis))
