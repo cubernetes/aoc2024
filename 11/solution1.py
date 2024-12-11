c037171075df0ff74c918b22bcc51aa7e12c1410
tosu <timo42@proton.me>
2024-12-11 10:03:20 +0100

    11 s01 + s02

---
+++ 11/solution1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from functools import lru_cache
+@lru_cache(200000)
+def recurse(stone: str, depth: int) -> int:
+    if depth == 0:
+        return 1
+    elif stone == '0':
+        return recurse('1', depth - 1)
+    elif len(stone) % 2 == 0:
+        return recurse(stone[:len(stone) // 2], depth - 1) \
+            + recurse(str(int(stone[len(stone) // 2:])), depth - 1)
+    else:
+        return recurse(str(int(stone) * 2024), depth - 1)
+print(sum(map(lambda stone: recurse(stone, 25), input().split())))
